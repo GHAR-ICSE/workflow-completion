23ecda050180945406bfe1224e353e36b0dfc1a8
Anonymous <>
2023-03-24 15:44:43 +0100

    Updated script

--- Code/Statistical-Tests/Wilcoxon.R
+++ Code/Statistical-Tests/Wilcoxon.R
@@ -10,21 +10,25 @@ res=list(Dataset=c(),Wilcoxon.p=c())
 
 
 # res$Dataset=c(res$Dataset,as.character("../Data/Wilcoxon/raw.csv"))
-# res$Wilcoxon.p=c(wilcox.test(raw$B4_T5,raw$B4_NGRAM,alternative="two.side",paired=TRUE)$p.value)
-# cliff.delta(raw$B4_T5,raw$B4_NGRAM)
+# res$Wilcoxon.p=c(res$Wilcoxon.p, wilcox.test(raw$B4_T5,raw$B4_NGRAM,alternative="two.side",paired=TRUE)$p.value)
+
 
 # res$Dataset=c(res$Dataset,as.character("../Data/Wilcoxon/abstracted.csv"))
-# res$Wilcoxon.p=c(wilcox.test(abstracted$B4_T5,abstracted$B4_NGRAM,alternative="two.side",paired=TRUE)$p.value)
+# res$Wilcoxon.p=c(res$Wilcoxon.p, wilcox.test(abstracted$B4_T5,abstracted$B4_NGRAM,alternative="two.side",paired=TRUE)$p.value)
+
 # cliff.delta(abstracted$B4_T5,abstracted$B4_NGRAM)
+# cliff.delta(raw$B4_T5,raw$B4_NGRAM)
 
 ########### COMMENT when running the Wilcoxon test for the BLEU-4 score ###########
 res$Dataset=c(res$Dataset,as.character("../Data/Wilcoxon/raw.csv"))
-res$Wilcoxon.p=c(wilcox.test(raw$ROUGE_T5,raw$ROUGE_NGRAM,alternative="two.side",paired=TRUE)$p.value)
-cliff.delta(raw$ROUGE_T5,raw$ROUGE_NGRAM)
+res$Wilcoxon.p=c(res$Wilcoxon.p, wilcox.test(raw$ROUGE_T5,raw$ROUGE_NGRAM,alternative="two.side",paired=TRUE)$p.value)
 
 res$Dataset=c(res$Dataset,as.character("../Data/Wilcoxon/abstracted.csv"))
-res$Wilcoxon.p=c(wilcox.test(abstracted$ROUGE_T5,abstracted$ROUGE_NGRAM,alternative="two.side",paired=TRUE)$p.value)
+res$Wilcoxon.p=c(res$Wilcoxon.p, wilcox.test(abstracted$ROUGE_T5,abstracted$ROUGE_NGRAM,alternative="two.side",paired=TRUE)$p.value)
+
 cliff.delta(abstracted$ROUGE_T5,abstracted$ROUGE_NGRAM)
+cliff.delta(raw$ROUGE_T5,raw$ROUGE_NGRAM)
+
 
 res=data.frame(res)
 res$Wilcoxon.p=p.adjust(res$Wilcoxon.p,method="holm")
